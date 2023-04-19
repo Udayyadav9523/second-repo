b0f56733ae8ded3778c682240e949c88fe7db5a8
Uday kumar <udayyadav9523@gmail.com>
2023-04-19 21:42:06 +0530

    first commit

---
+++ 1.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+int main(){
+    int a;
+    scanf("%d\n",a);
+    if(a>0){
+        printf("postive ");
+    }if (a<0){
+        printf("negative");
+
+    }if(a==0){
+        printf("zero");
+    }
+    
+      
+    return 0;
+}
